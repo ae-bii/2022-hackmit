a8d744e3e396f1965be9fb7f0dac0dc113d14887
kevinhuang-ucsd <zihenghuang4@gmail.com>
2022-10-01 19:45:59 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import "./App.css";
 import CommunitySlider from "./CommunitySlider";
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <CommunitySlider />
+      <Footer />
     </div>
   );
 };
