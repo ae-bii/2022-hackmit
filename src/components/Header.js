e31ef961ba83ccde2349726d5ddd542ee1275db0
kevinhuang-ucsd <zihenghuang4@gmail.com>
2022-10-01 17:39:19 -0400

    imported libraries

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import { Navbar, Nav } from 'rsuite';
+import HomeIcon from '@rsuite/icons/legacy/Home';
+import CogIcon from '@rsuite/icons/legacy/Cog';
+
+const App = () => (
+  <Navbar>
+    <Navbar.Brand href="#">RSUITE</Navbar.Brand>
+    <Nav>
+      <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
+      <Nav.Item>News</Nav.Item>
+      <Nav.Item>Products</Nav.Item>
+      <Nav.Menu title="About">
+        <Nav.Item>Company</Nav.Item>
+        <Nav.Item>Team</Nav.Item>
+        <Nav.Menu title="Contact">
+          <Nav.Item>Via email</Nav.Item>
+          <Nav.Item>Via telephone</Nav.Item>
+        </Nav.Menu>
+      </Nav.Menu>
+    </Nav>
+    <Nav pullRight>
+      <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+    </Nav>
+  </Navbar>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
