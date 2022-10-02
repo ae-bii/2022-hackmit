a2ff31d6bd2abe40a5ec505b6ee688cbf80f73c7
Anupam Bhakta <anupambhakta1@gmail.com>
2022-10-01 22:15:48 -0400

    updated sliders

--- src/components/CommunitySlider.js
+++ src/components/CommunitySlider.js
@@ -4,13 +4,13 @@ import React from "react";
 
 const CommunitySlider = () => {
   return (
-    <Carousel wrapAround="true" cellAlign='center'>
-      <img src="https://via.placeholder.com/1920x483" />
-      <img src="https://via.placeholder.com/1920x483" />
-      <img src="https://via.placeholder.com/1920x483" />
-      <img src="https://via.placeholder.com/1920x483" />
-      <img src="https://via.placeholder.com/1920x483" />
-      <img src="https://via.placeholder.com/1920x483" />
+    <Carousel autoplay="true" autoplayInterval={3000} renderCenterLeftControls="null" renderCenterRightControls="null" wrapAround="true" cellAlign='center'>
+      <img alt="" src="https://via.placeholder.com/1920x483" />
+      <img alt="" src="https://via.placeholder.com/1920x483" />
+      <img alt="" src="https://via.placeholder.com/1920x483" />
+      <img alt="" src="https://via.placeholder.com/1920x483" />
+      <img alt="" src="https://via.placeholder.com/1920x483" />
+      <img alt="" src="https://via.placeholder.com/1920x483" />
     </Carousel>
   );
 };
--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,18 +1,19 @@
 import ModelSlider from "./ModelSlider";
 import PaperSlider from "./PaperSlider";
+import "./Resources.css";
 
 const Resources = () => (
     <div className="container" style={{display: "flex", flexDirection: "row"}}>
         <div className="models_papers" style={{width: "50%"}}>
             <div>
-                <h2>
+                <h2 class="models">
                     Models
                 </h2>
                 <ModelSlider />
             </div>
             <div>
-            <h2>
-                Paper
+                <h2 class="papers">
+                    Paper
                 </h2>
                 <PaperSlider />
             </div>
@@ -20,11 +21,11 @@ const Resources = () => (
         <div className="pipelines" style={{width: "50%"}}>
             <p>Pipelines</p>
             <div className="piplines" style={{backgroundColor: "grey"}}>
-                <a class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>Upload Your Data</a>
-                <a class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>Select Preprocesing Methods</a>
-                <a class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>Select Model</a>
-                <a class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>Select Evaluation Methods</a>
-                <a class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>View Results</a>
+                <a href="/" class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>Upload Your Data</a>
+                <a href="/" class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>Select Preprocesing Methods</a>
+                <a href="/" class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>Select Model</a>
+                <a href="/" class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>Select Evaluation Methods</a>
+                <a href="/" class="waves-effect waves-light btn" style={{left: "30%", width: "40%", display: "block", marginBottom: "10px", backgroundColor: "#003865"}}>View Results</a>
             </div>        
             
         </div>
