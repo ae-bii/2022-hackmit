86c00bb7b93003fe28ba71d34ecfae599ae7d418
kevinhuang-ucsd <zihenghuang4@gmail.com>
2022-10-01 20:09:24 -0400

    added Resources.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './components/Header';
 import "./App.css";
 import CommunitySlider from "./components/CommunitySlider";
 import Footer from './components/Footer';
+import Resources from './components/Resources';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <CommunitySlider />
+      <Resources />
       <Footer />
     </div>
   );
---
+++ src/components/Resources.js
@@ -0,0 +1,12 @@
+const Resources = () => (
+    <div className="container" style={{display: "flex", flexDirection: "row"}}>
+        <div className="models_papers" style={{width: "70%"}}>
+
+        </div>
+        <div className="pipelines" style={{width: "30%"}}>
+
+        </div>
+    </div>
+  );
+  
+  export default Resources;
